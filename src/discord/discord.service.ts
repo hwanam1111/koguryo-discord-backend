850c1cec9904483cbd7b492048edd3bd1434f703
Kim LeeJun <hwanam1111@naver.com>
2022-10-21 17:40:03 +0900

    Add: auto params logs

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -22,6 +22,14 @@ export class DiscordService {
 
   async discordAuth({ code }: DiscordAuthQuery): Promise<DiscordAuthOutput> {
     try {
+      console.log('>> auth params logs : ', {
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        code,
+        grant_type: 'authorization_code',
+        redirect_uri: `${process.env.FRONT_END_URL}`,
+        scope: 'identify',
+      });
       const auth = await axios({
         url: 'https://discord.com/api/oauth2/token',
         method: 'POST',
